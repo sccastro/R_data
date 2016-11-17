0b10bfbf1b4b533d5be4e6ce034d7e900799afa9
sccastro <cac200222@gmail.com>
2016-11-17 12:20:34 -0700

    Updated LBA code
    This code works for reading in all the DRT data and being able to use
    it.

--- Nature_DRT/Load_Data.R
+++ Nature_DRT/Load_Data.R
@@ -1,76 +1,63 @@
-# Get Data from Dropbox ---------------------------------------------------
 
+rm(list = ls())
+require(doBy)
 require(dplyr)
-
-# Get all files and apply functions to them -------------------------------
-
-#Get filenames
+setwd("Documents/R_Data/Nature_DRT")
 files <- list.files(path="DRT_all", pattern="*.csv", full.names=T, recursive=FALSE)
-#Get Subject IDs and conditions
 subids <- regmatches(files, regexpr("\\d{3}", files))
-condition <- c()
-for (i in files) condition[i] <- regmatches(i, regexpr("[PT][roe][a-z]", i))
+condition <- as.character(lapply(strsplit(files, "_"), "[",4))
 
-#vectorize condition and subids
-condition <- unname(condition, force = FALSE)
-condition <- as.vector(condition)
-
-subids <- as.vector(subids)
-
-newcols <- data.frame(condition,subids)
-
-
-
-# Summary of the data types to merge --------------------------------------
-
-#The 3 groups of data are condition, subids, and files. They are all characters with 78 elements. 
-#I want to open the file associated with the filename in files, and append subids and condition to 
-# every row in the file. Then I want to combine all of the files into one master file.
-
-
-newcols[7:10,1]
-str(newcols)
-
-str(files)
-
-
-
-
-
-
-
-
-#List of files with all data
-
-#This creates a list of data.frames. datalist is a large matrix of 1092 elements
-datalist <- sapply(files, function(x,...){
-  read.csv(file=x,header=T)
+for (i in 1:length(files)) {
+  infile <- read.csv(files[i])
+  infile$subids <- as.factor(subids[i])
+  infile$condition <- as.factor(condition[i])
   
-  # I tried looping through the newcols data.frame and appending the element to a file. 
-  #It didn't work 
-  # for (i in newcols) datalist[[i]]$condition <- rep(newcols[i,1], nrow(datalist[[i]]$UniqueID))
-  # for (j in newcols) onefile$subid <- rep(newcols[i,2], nrow(onfile))
-  
-  })
-
-
-
-
-
-
-
-# merge all of the files together
-for (i in 1:78) {
-  bind_cols(datalist[[i]],data.frame(condition[[i]]))
+  if(!exists("combined.data")) {
+    combined.data <- infile
+  }
+  else {
+    combined.data <- rbind(combined.data,infile)
+  }
 }
 
-datalist[[1]]$UniqueID
 
-# combined <- bind_rows(datalist)
-# summary(combined)
+# Explore data ------------------------------------------------------------
+
+summary(combined.data)
+str(combined.data)
 
 
+# Clean up dataset --------------------------------------------------------
 
-class(datalist[[1]])
 
-datalist[[1]]
+clean.data <- combined.data %>%
+  select(subid = subids, condition, isPractice,
+         r1g0 = LED.Red..1.Red.0.Green., rt = Response.Time)
+
+  
+levels(clean.data$condition) <- list(PreTesting = ("PreTesting"), 
+                            Testing = ("Testing"),
+                            Posttesting = c("Posttesting","Postsetting","Posttetting"))
+clean.data$isPractice <- factor(clean.data$isPractice)
+levels(clean.data$isPractice) <- c("no","yes")
+clean.data$r1g0 <- factor(clean.data$r1g0)
+levels(clean.data$r1g0) <- c("g", "r")
+clean.data$subid <- factor(clean.data$subid)
+
+clean.data$isPractice[1:100]
+clean.data$r1g0[1:100]
+
+
+# Descriptives ------------------------------------------------------------
+
+
+summaryBy(rt ~ subid + condition + r1g0, data = clean.data)
+
+clean.data %>%
+  group_by(subid, condition, r1g0) %>%
+  summarise(total.count = n())
+
+
+# Plot Reaction Times -----------------------------------------------------
+
+
