dfb31248cf5a3f27c01cd986c2188d533073e865
sccastro <cac200222@gmail.com>
2016-12-05 11:22:49 -0700

    Updates to Mitch and Rachel's Studies
    Updated work, completed Nature DRT plots

--- MitchHonors/MitchCleanData11_1.R
+++ MitchHonors/MitchCleanData11_1.R
@@ -1,5 +1,9 @@
 # Clear/setup workspace ---------------------------------------------------
 rm(list=ls()); par(mfrow = c(1,1))
+source("~/Documents/R_data/Nature_DRT/Load_Data.R")
+
+library(dplyr)
+library(ggplot2)
 
 my.axis.font<-theme(axis.title.x = element_text(size=18), axis.title.y = element_text(size=18),
                     axis.text.x = element_text(size=18), axis.text.y = element_text(size=18), 
@@ -7,10 +11,9 @@ my.axis.font<-theme(axis.title.x = element_text(size=18), axis.title.y = element
                     legend.title=element_text(size=14),legend.text=element_text(size=14))
 
 
-library(dplyr)
-library(ggplot2)
+
 #Other users of this script will have to set their own working directory
-setwd("C:/Users/u1010240/Desktop/Studies/R_data/MitchHonors")
+setwd("~/Documents/R_data/MitchHonors")
 
 
 # Get all files and apply functions to them -------------------------------
@@ -30,7 +33,7 @@ combined <- do.call(rbind, datalist)
 
 
 #rename the sub column subid (matches a function sub())
-combined <- rename(combined, subid = sub)
+combined <- dplyr::rename(combined, subid = sub)
 # Clean Data --------------------------------------------------------------
 
 #create a new column that finds the letter in the subid column and creates the condition.
@@ -42,31 +45,80 @@ combined$condition <- factor(ifelse(grepl("p", combined$subid, ignore.case = T),
 clean <- combined %>%
   select(subid,condition,targcol,corr,rt1,rt2) %>%
   na.omit() %>%
+  arrange(!desc(subid)) %>%
+  filter(rt1 > 300 & rt1 < 6000, rt2 > 150 & rt2 < 3000)
+
+cleanac <- combined %>%
+  select(subid,condition,targcol,corr,rt1,rt2) %>%
+  na.omit() %>%
   arrange(!desc(subid))
+#Get the letters out of subid
+cleanac$subid <- factor(gsub('[a-z]+', '', cleanac$subid))
+cleanac$condition <- factor(cleanac$condition, levels = c("practice","white","green","mixed"))
+
+
 
 #Get the letters out of subid
 clean$subid <- factor(gsub('[a-z]+', '', clean$subid))
 
+clean$condition <- factor(clean$condition, levels = c("practice","white","green","mixed"))
 
 
 # Descriptive Stats -------------------------------------------------------
+#Colors for plots
+#Create a custom color scale
+myColors <- c("red","grey","green","purple")
+names(myColors) <- levels(clean$condition)
+colScale <- scale_colour_manual(name = "conditions",values = myColors)
+
 
 #Percentage correct by group
 acc.mean <- clean %>%
   group_by(condition) %>%
   summarise(perc_corr = mean(corr)*100)
 
-#RT
+datac <- summarySEwithin(cleanac, measurevar ="corr", withinvars = c("condition"), idvar = "subid")
 
 
-accplot <- ggplot(acc.mean, aes(x = as.factor(condition), y=perc_corr, colour = perc_corr)) +
-  geom_point(size=5) + ylim(90,100) + theme_minimal() +  my.axis.font +
+accplot <- ggplot(datac, aes(x = as.factor(condition), y=corr)) +
+  geom_bar(stat = "identity", position = "dodge") + theme_minimal() +
+  my.axis.font + geom_errorbar(position=position_dodge(.9), width = .25,
+                               aes(ymin=corr-ci, ymax=corr+ci)) +
   xlab("Condition")
 accplot
 
-rtplot <- ggplot(clean, aes(x = as.factor(condition), y=rt1, colour = condition)) + 
-  geom_boxplot() + ylim(0,1300) + theme_minimal() + my.axis.font + xlab("Condition") 
-# + guides(colour=FALSE) +
-# scale_x_discrete(breaks = 1:2, labels = c("Green","White"))
-#can add target color --(colour = targcol, group=targcol)
-rtplot
+
+
+#RT
+rthist1 <- ggplot(data=clean, aes(x=rt1, colour = as.factor(condition))) + 
+  stat_density(position="identity", geom="line", aes(colour = as.factor(condition))) + 
+  ggtitle("Reaction Time \n by Condition \n in Selecting Target") + theme_minimal() +
+  colScale
+rthist1
+
+rthist2 <- ggplot(data=clean, aes(x=rt2, colour = as.factor(condition))) + 
+  stat_density(position="identity", geom="line", aes(colour = as.factor(condition))) + 
+  colScale +
+  ggtitle("Reaction Time \n by Condition \n in Selecting Target") + theme_minimal()
+rthist2
+
+#Box and whisker plots
+
+rtplot1 <- ggplot(clean, aes(x = as.factor(condition), y=rt1, colour = condition)) + 
+  geom_violin(draw_quantiles = c(0.25, 0.5, 0.75), size = 1) + ylim(0,6000) + 
+  stat_boxplot(geom ='errorbar', width = 0.1)+
+  geom_boxplot(width = 0.2)+
+  theme_minimal() + my.axis.font + xlab("Condition") +
+  colScale + theme(legend.position="none")
+rtplot1
+
+rtplot2 <- ggplot(clean, aes(x = as.factor(condition), y=rt2, colour = condition)) + 
+  geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) + ylim(0,3000) +
+  stat_boxplot(geom ='errorbar', width = 0.1)+
+  geom_boxplot(width = 0.2)+
+  theme_minimal() + my.axis.font + xlab("Condition") +
+  theme(legend.position="none") + colScale
+rtplot2
+
+
+
--- Nature_DRT/Nature_Report_Plots.R
+++ Nature_DRT/Nature_Report_Plots.R
@@ -7,14 +7,7 @@ combined.data <- CombineData()
 clean.data <- CleanData(combined.data)
 
 
-
-
-
-
-
 # Descriptives ------------------------------------------------------------
-str(clean.data)
-
 summaryBy(rt ~ subid + condition + r1g0, data = clean.data)
 
 
@@ -26,23 +19,57 @@ clean.data %>%
 # Calculate DV's -----------------------------------------------------
 #Create accuracy
 clean <- clean.data %>%
-  filter(isPractice == "no") %>%
+  filter(isPractice == "no", pressCount == 1 | pressCount == 0) %>%
   group_by(subid,condition,r1g0) %>%
   dplyr::mutate(falsealarm = ifelse(pressCount == 1 & r1g0 == "r", 1,0)) %>%
   dplyr::mutate(miss = ifelse(pressCount == 0 & r1g0 == "g", 1,0)) %>%
   dplyr::mutate(totalerrors = miss + falsealarm)
+
+clean.mean <- clean %>%
+  group_by(subid,condition, r1g0) %>%
+  dplyr::summarise(perc_error = mean(totalerrors)*100)
+
+clean.rt <- clean %>%
+  select(subid,condition,r1g0,rt,falsealarm) %>%
+  filter(rt >= 150 & rt <= 2000)
+
+# Outliers ----------------------------------------------------------------
+source("https://raw.githubusercontent.com/talgalili/R-code-snippets/master/boxplot.with.outlier.label.r") # Load the function
+
+
+boxplot.with.outlier.label(clean.rt$rt~clean.rt$condition*clean.rt$r1g0,
+                           label_name = clean.rt$subid)
+
+
+notwanted <- c(2,3,5,8,14,15,20,22,23)
+
+subjectsremoved <- clean %>%
+  filter(!as.integer(subid) %in% notwanted)
+
+levels(subjectsremoved$subid) <- droplevels(subjectsremoved$subid)
+
+
+#rthist
+medlines <- clean.rt %>%
+  group_by(condition) %>%
+  dplyr::summarise(rt.med = median(rt))
+
+ggplot(data.frame(clean.rt), aes(rt, fill = condition)) + 
+  geom_density(alpha=.8) + xlim(0,2000) + 
+  geom_vline(data = as.data.frame(medlines), aes(xintercept=medlines$rt.med, color=condition),
+             linetype="dashed", size=1)
+
 
 
 # Get some Error Bars -----------------------------------------------------
 datac <- summarySEwithin(clean, measurevar ="totalerrors", withinvars = c("condition","r1g0"), idvar = "subid")
 datac
 
-datart <- summarySEwithin(clean, measurevar ="rt", withinvars = c("condition","totalerrors"), idvar = "subid")
+datart <- summarySEwithin(clean.rt, measurevar ="rt", withinvars = c("condition","falsealarm"), idvar = "subid")
 datart
 
 
 # Make Plots --------------------------------------------------------------
-
 
 acc.mean <- clean %>%
   group_by(condition, r1g0) %>%
@@ -61,46 +88,69 @@ accplot2 <- ggplot(datac, aes(x = condition, y=1-totalerrors, fill = r1g0)) +
   ggtitle("Percent \n Correct Responses in a \n Go-NoGo DRT Task \n")
 accplot2
 
+datac2 <- datac %>% 
+  group_by(condition) %>%
+  dplyr::summarise(totalerrors = mean(totalerrors),ci = mean(ci))
+  
+
+accplot3 <- ggplot(datac2, aes(x = as.factor(condition), y=1-totalerrors)) +
+  geom_bar(stat = "identity", position = "dodge") + theme_minimal() +
+  my.axis.font + geom_errorbar(position=position_dodge(.9), width = .25, 
+                aes(ymin=1-totalerrors-ci, ymax=1-totalerrors+ci)) +
+  xlab("Condition") + coord_cartesian(ylim=c(.7,1))
+accplot3
 
 
+# Reaction Time Plots -----------------------------------------------------
+
+rtplot1 <- ggplot(datart, aes(x = as.factor(falsealarm), 
+                              fill = condition,y=rt_norm)) +
+  geom_bar(stat = "identity", position = "dodge") + theme_minimal() +
+  my.axis.font() + geom_errorbar(position=position_dodge(.9), width = .25, 
+                               aes(ymin=rt_norm-ci, ymax=rt_norm+ci)) +
+  scale_x_discrete("False Alarm", labels = c("incorrect","correct"))
+rtplot1
 
 
 # Simple Statistics -------------------------------------------------------
-clean.mean <- clean %>%
-  group_by(subid,condition, r1g0) %>%
-  dplyr::summarise(perc_error = mean(totalerrors)*100)
 
 summaryBy(perc_error~condition,data=as.data.frame(clean.mean),FUN=function(x) {any(is.na(x))})
 
-acc.lm <- with(as.data.frame(clean.mean), lmer(perc_error ~ condition  + (1|subid)),na.omit()) 
+acc.lm <- with(as.data.frame(clean.mean), lmer(perc_error ~ condition * r1g0 + (1|subid)),na.omit()) 
 
 summary(acc.lm)
 
 # Linear mixed model fit by REML ['lmerMod']
-# Formula: perc_error ~ condition + (1 | subid)
+# Formula: perc_error ~ condition * r1g0 + (1 | subid)
 # 
-# REML criterion at convergence: 1412.5
+# REML criterion at convergence: 1385.8
 # 
 # Scaled residuals: 
 #   Min      1Q  Median      3Q     Max 
-# -1.5859 -0.3819 -0.2061 -0.1834  3.5690 
+# -2.0552 -0.4722 -0.2102  0.0404  3.4156 
 # 
 # Random effects:
 #   Groups   Name        Variance Std.Dev.
-# subid    (Intercept) 107.1    10.35   
-# Residual             589.6    24.28   
+# subid    (Intercept) 111.6    10.56   
+# Residual             558.6    23.63   
 # Number of obs: 153, groups:  subid, 26
 # 
 # Fixed effects:
-#   Estimate Std. Error t value
-# (Intercept)            9.3816     4.0006   2.345
-# conditionTesting       0.2709     4.8447   0.056
-# conditionPosttesting   0.2539     4.8191   0.053
+#                             Estimate Std. Error t value
+# (Intercept)                  10.144      5.170   1.962
+# conditionTesting              5.171      6.699   0.772
+# conditionPosttesting          8.269      6.627   1.248
+# r1g0r                        -1.502      6.685  -0.225
+# conditionTesting:r1g0r       -9.593      9.413  -1.019
+# conditionPosttesting:r1g0r  -16.053      9.362  -1.715
 # 
 # Correlation of Fixed Effects:
-#             (Intr) cndtnT
-# condtnTstng -0.613       
-# cndtnPsttst -0.617  0.509
+#   (Intr) cndtnT cndtnP r1g0r  cnT:10
+# condtnTstng -0.648                            
+# cndtnPsttst -0.655  0.506                     
+# r1g0r       -0.646  0.499  0.504              
+# cndtnTst:10  0.459 -0.710 -0.358 -0.710       
+# cndtnPst:10  0.462 -0.356 -0.706 -0.714  0.507
 
 with(as.data.frame(clean.mean), lmer(perc_error ~ condition  + (1|subid)),na.omit()) %>% anova()
 
@@ -116,4 +166,13 @@ with(as.data.frame(clean.mean), lmer(perc_error ~ condition  + (1|subid)),na.omi
 ggplot(data = combined.data, aes(x = UniqueID, y = Response.Time)) + 
   geom_point() +
   geom_smooth(method = "lm")
+
+
+# Full Model --------------------------------------------------------------
+
+full.lm <- with(as.data.frame(), lmer(perc_error ~ condition * r1g0 + (1|subid)),na.omit())
+
+
+
+
 
